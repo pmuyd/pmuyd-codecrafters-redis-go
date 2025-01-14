a0df3d056a4bc8b46e08334b840ad06d66728fe8
pmuyd <uy.pham@wsu.edu>
2025-01-13 19:09:40 -0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -11,14 +11,24 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
+	// Bind to port 6379
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error accepting connection:", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Respond with PONG, ignoring client input for now
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing response:", err.Error())
 		os.Exit(1)
 	}
 }
